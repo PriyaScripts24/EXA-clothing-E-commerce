c59656f22b009a81a14d7149783c753b42d88414
priya09-collab <priyadev0912@gmail.com>
2025-01-27 15:18:23 +0530

    apidata

---
+++ pages/apidata.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <div v-for="(value, key) in users" :key="key">{{ key }} :{{ value }}</div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: {},
+    };
+  },
+  methods: {
+    async dataType() {
+      const response = await axios.get("https://api.genderize.io/?name=luc");
+      this.users = response.data;
+    },
+  },
+  mounted() {
+    this.dataType();
+  },
+};
+</script>
